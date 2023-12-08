f3c278b39afb8a9db70b84fab2c94228e3caf015
Harshit Mishra <harrymishra831@gmail.com>
2023-12-08 20:29:03 +0530

    Added authentication system

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const todoRouter = require('./routes/todoRoutes');
+const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors())
 app.use(express.json());
 
 app.use('/todos', todoRouter)
+app.use('/auth', authRouter)
 
 
 module.exports = app;
